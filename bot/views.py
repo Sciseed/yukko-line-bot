907d9745b7f851bbca99d7dbde219bd12d8a0cdd
Kosuke Suzuki <kosuke.suzuki@happyelements.co,jp>
2016-09-20 18:45:27 +0900

    全機能つけた

--- bot/views.py
+++ bot/views.py
@@ -38,11 +38,12 @@ def post_text(send_to, content):
         'X-Line-ChannelSecret': '37df4c7d811276edf33c741471f9f906',
         'X-Line-Trusted-User-With-ACL': 'ufbb1954b3357ab82f558b1e695096212'
     }
-    output = mizu_res[0]['a'][0]
-    # if 'わかりませんでした' in docomo_res_q['message']['textForDisplay']:
-    #     output = docomo_res['utt']
-    # else:
-    #     output = docomo_res_q['message']['textForDisplay']
+    if mizu_res[0]['a'][0] != []:
+          output = mizu_res[0]['a'][0]
+    elif 'わかりませんでした' in docomo_res_q['message']['textForDisplay']:
+        output = docomo_res['utt']
+    else:
+        output = docomo_res_q['message']['textForDisplay']
     payload = {
         'toChannel': 1383378250,
         'eventType': '138311608800106203',
