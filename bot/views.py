8b7b1300ad5ff572169d564d8adb4c1514d6d2d2
Kosuke Suzuki <kosuke.suzuki@happyelements.co,jp>
2016-09-23 10:09:14 +0900

    edit mecab_test

--- bot/views.py
+++ bot/views.py
@@ -29,7 +29,7 @@ def post_text(send_to, content):
     'q': content
     }
     docomo_res_q = json.loads(requests.get(DOCOMO_ENDPOINT, params=options).text)
-    #q = {'q': content}
+    q = {'q': content}
     mizu_res = json.loads(requests.get(MIZU_ENDPOINT, params=q).text) #qに対するaが返される
     headers = {
         'Content-Type': 'application/json; charset=UTF-8',
