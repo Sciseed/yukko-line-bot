26bb4069be35d0cd53062f61f022232fc8524b62
Kosuke Suzuki <kosuke.suzuki@happyelements.co,jp>
2016-09-23 12:33:59 +0900

    edit mecab_test

--- bot/views.py
+++ bot/views.py
@@ -28,12 +28,11 @@ def post_text(send_to, content):
     #雑談
     docomo_client = doco.client.Client(apikey=DOCOMO_API_KEY)
     docomo_res = docomo_client.send(utt=content,apiname='Dialogue')
+    options = {
+      'APIKEY': '6255615075614d4a3455552f57546d583366686d3332314746456e6e49714a49464d43325a667561685a33',
+      'q': content
+    }
     docomo_res_q = json.loads(requests.get(DOCOMO_ENDPOINT, params=options).text)
-    #質問
-    # options = {
-    # 'APIKEY': '6255615075614d4a3455552f57546d583366686d3332314746456e6e49714a49464d43325a667561685a33',
-    # 'q': content
-    # }
     #q = {'q': content}
     #mizu_res = json.loads(requests.get(MIZU_ENDPOINT, params=q).text) #qに対するaが返される
     headers = {
@@ -78,7 +77,7 @@ def post_text(send_to, content):
     req = requests.post(ENDPOINT, headers=headers, data=json.dumps(payload))
     print (json.dumps(payload))
     print (json.dumps(headers))
-    print('request')
+    print(payload['content']['text'])
     print(req.__dict__)
 
 def post_question(send_to, question):
