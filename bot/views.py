d1563a50cc25213f8b743a2f748011525540ec9a
Kosuke Suzuki <kosuke.suzuki@happyelements.co,jp>
2016-09-23 12:50:18 +0900

    mizuを統合

--- bot/views.py
+++ bot/views.py
@@ -68,11 +68,11 @@ def post_text(send_to, content):
         }
     }
     print('request')
-    req = requests.post(ENDPOINT, headers=headers, data=json.dumps(payload))
+    #req = requests.post(ENDPOINT, headers=headers, data=json.dumps(payload))
     print (json.dumps(payload))
     print (json.dumps(headers))
     print(payload['content']['text'])
-    print(req.__dict__)
+    #print(req.__dict__)
 
 def post_question(send_to, question):
     options = {
