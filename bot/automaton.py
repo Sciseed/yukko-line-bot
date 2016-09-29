ed4c5cc61cf5333d6b7b7b380f1e4679ff110206
Kosuke Suzuki <kosuke.suzuki@happyelements.co,jp>
2016-09-29 15:24:38 +0900

    test

--- bot/automaton.py
+++ bot/automaton.py
@@ -1,10 +1,10 @@
 import MeCab
 import sys
 
-# def main():
-#     sentence = "自己分析をするのですか？"
-#     answertype = make_flag(sentence)
-#     print(answertype)
+def main():
+  sentence = input()
+  flag = make_flag(sentence)
+  print(flag)
 
 def analyze(txt):
   m = MeCab.Tagger("-Ochasen -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
@@ -36,84 +36,165 @@ def make_flag(sentence):
     morph_dics = dicnize(morph)
     state = 'init'
     answertype = 'None'
+    answertype_list = ['自己分析', '面接', '企業の探し方','企業研究', '試験', '就活', 'リクルーター', '内定辞退', '就活の時期と主にやること', '職種軸', '業種軸', '質問', '企業の強み']
+
+
+    s1_list = ['自己分析', '仕事', 'キャリアプラン']
+    s2_list = ['面接', '個人面接', 'グループ面接','グループ面談','一次面接', '二次面接']
+    s3_list = ['企業研究', '業界研究']
+    s4_list = ['試験', 'webテスト', 'ウェブテスト', '試験対策', '筆記試験', 'SPI', 'Webテスト']
+    s5_list = ['就活']
+    s6_list = ['リクルーター']
+    s7_list = ['内定', '内定辞退']
+    s8_list = ['就活', '就職活動']
+    s9_list = []
+    s10_list = ['軸']
+    s11_list = ['逆質問']
+    s12_list = ['企業']
+
+    s1_1_list = ['方法', '仕方', 'やり方']
+    s2_1_list = ['心構え', '対策', 'コツ', 'アドバイス', '注意点', '気をつけること','よく聞かれること']
+    s3_1_list = ['企業', '業界', '探索', '探し方']
+    s3_1_another_list = ['企業','業界', '探索', '探し方']
+    s4_1_list = ['対策', '勉強']
+    s5_1_list = ['タイムライン', 'スケジュール']
+    s6_1_list = ['どの']
+    s7_1_list = ['辞退', '断る']
+    s8_1_list = ['進め方', 'スケジュール', 'タイムライン']
+    s9_1_list = ['企業', '業界', '職種']
+    s10_1_list = ['仕事', '自己分析']
+    s11_1_list = ['面接', '最後', '逆質問']
+    s12_1_list = ['情報', '強み']
+    weights = [0] * len(answertype_list)
     for x in morph_dics:
-      #print(x)
-      # print(x["surface"])
+      key = x['surface']
+      #初期層
       if state == 'init':
-        if x['surface'] == '自己分析':
-          answertype = 'Self-analysis'
-        elif x['surface'] == '面接':
-          answertype = 'Interview'
-        elif x['surface'] == '企業研究':
-          answertype = 'Coporate-research'
-        elif x['surface'] == '試験':
-          answertype = 'Test'
-        elif x['surface'] == '就活':
-          answertype = 'Job-hunting'
-        elif x['surface'] == 'リクルーター':
-          answertype = 'recruiter'
-        elif x['surface'] == '内定辞退':
-          answertype = 'Bow-out'
-        elif x['surface'] == '職種軸':
-          answertype = 'Occup-type'
-        elif x['surface'] == '質問':
-          answertype = 'Question'
-        elif x['surface'] == '強み':
-          answertype = 'Strongth'
+        if key in s1_list:
+          weights[answertype_list.index('自己分析')] += 1
+          state = 's11'
+        elif key in s2_list:
+          weights[answertype_list.index('面接')] += 1
+          state = 's21'
+        elif key in s3_list:
+          weights[answertype_list.index('企業の探し方')] += 1
+          state = 's31'
+        # elif (key=='いつ'):
+        #   weights[answertype_list.index('自己分析')] += 0.2
+        #   state = 's111'
+        elif key in s4_list:
+          weights[answertype_list.index('試験')] += 1
+          state = 's41'
+        elif key in s5_list:
+          weights[answertype_list.index('就活')] += 1
+          state = 's51'
+        elif key in s6_list:
+          weights[answertype_list.index('リクルーター')] += 1
+          state = 's61'
+        elif key in s7_list:
+          weights[answertype_list.index('内定辞退')] += 1
+          state = 's71'
+        elif key in s8_list:
+          weights[answertype_list.index('就活の時期と主にやること')] += 1
+          state = 's81'
+        elif key in s9_list:
+          weights[answertype_list.index('職種軸')] += 1
+          state = 's91'
+        # elif key == '軸':
+        #   weights[answertype_list.index('業種軸')] += 1
+        #   state = 's101'
+        elif key in s11_list:
+          weights[answertype_list.index('質問')] += 1
+          state = 's111'
+        elif key in s12_list:
+          weights[answertype_list.index('企業の強み')] += 1
+          state = 's121'
         else:
           continue
-    print(answertype)
-    return answertype
 
-def mecab_reader(mecabfile):
-    sentences = []
-    sentence = []
-    for line in mecabfile:
-        if line == "EOS\n":
-            if len(sentence) > 0:
-                sentences.append(sentence)
-            sentence = []
+      #第２層
+      elif state == 's11':
+        if key in s3_1_list:
+          weights[answertype_list.index('企業研究')] += 1
+        elif key in s1_1_list:
+          weights[answertype_list.index('自己分析')] += 1
         else:
-            surface, features = line.split("\t")
-            features = features.split(",")
-            dic = {
-                'surface': surface,
-                'base': features[6],
-                'pos': features[0],
-                'pos1': features[1]
-            }
-            sentence.append(dic)
-    return sentences
-
-def tabbed_str_to_dict(tabbed_str: str) -> dict:
-    """
-    例えば「次第に   シダイニ    次第に   副詞-一般   」のようなタブ区切りで形態素を表す文字列をDict型に変換する.
-    :param tabbed_str タブ区切りで形態素を表す文字列
-    :return Dict型で表された形態素
-    """
-    elements = tabbed_str.split()
-    if 0 < len(elements) < 4:
-        return {'surface': elements[0], 'base': '', 'pos': '', 'pos1': ''}
+          continue
+      elif state == 's21':
+        if key in s2_1_list:
+          weights[answertype_list.index('面接')] += 1
+        else:
+          continue
+      elif state == 's31':
+        if key in s1_1_list:
+          weights[answertype_list.index('自己分析')] += 1
+        elif key in s3_1_another_list:
+          weights[answertype_list.index('企業の探し方')] += 1
+        else:
+          continue
+      elif state == 's41':
+        if key in s4_1_list:
+          weights[answertype_list.index('試験')] += 1
+        else:
+          continue
+      elif state == 's51':
+        if key in s5_1_list:
+          weights[answertype_list.index('就活')] += 1
+        elif key in s8_1_list:
+          weights[answertype_list.index('就活の時期と主にやること')] += 1
+        else:
+          continue
+      elif state == 's61':
+        if key in s6_1_list:
+          weights[answertype_list.index('リクルーター')] += 1
+        else:
+          continue
+      elif state == 's71':
+        if key in s7_1_list:
+          weights[answertype_list.index('内定辞退')] += 1
+        else:
+          continue
+      elif state == 's81':
+        if key in s8_1_list:
+          weights[answertype_list.index('就活の時期と主にやること')] += 1
+        else:
+          continue
+      elif state == 's91':
+        if key in s9_1_list:
+          weights[answertype_list.index('職種軸')] += 1
+        elif key in s10_1_list:
+          weights[answertype_list.index('業種軸')] += 1
+        else:
+          continue
+      elif state == 's111':
+        if key in s11_1_list:
+          weights[answertype_list.index('質問')] += 1
+        else:
+          continue
+      elif state == 's121':
+        if key in s12_1_list:
+          weights[answertype_list.index('企業の強み')] += 1
+        else:
+          continue
+      # elif state == 's111':
+      #   if key in s111_list:
+      #     weights[answertype_list.index('自己分析')] += 0.3
+      #     state = 's112'
+      #   else:
+      #     continue
+      # elif state == 's112':
+      #   if key in s1111_list:
+      #     weights[answertype_list.index('自己分析')] += 0.6
+      #   else:
+      #     continue
+    answertype = answertype_list[weights.index(max(weights))]
+    if any(weights):
+      answertype_num = weights.index(max(weights))
     else:
-        return {'surface': elements[0], 'base': elements[1], 'pos': elements[2], 'pos1': elements[3]}
+      answertype_num = 'X'
+    # print(weights)
+    return answertype_num
 
-
-def morphemes_to_sentence(morphemes: list) -> list:
-    """
-    Dict型で表された形態素のリストを句点毎にグルーピングし、リスト化する.
-    :param morphemes Dict型で表された形態素のリスト
-    :return 文章のリスト
-    """
-    sentences = []
-    sentence = []
-
-    for morpheme in morphemes:
-        sentence.append(morpheme)
-        if morpheme['pos1'] == '記号-句点':
-            sentences.append(sentence)
-            sentence = []
-
-    return sentences
 
 if __name__=='__main__':
     main()
