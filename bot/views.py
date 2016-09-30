1e48e839e92df2e9fc798d7bba94bd501420fd9d
canolead <canolead@gmail.com>
2016-09-30 18:35:16 +0900

    Modify the carousel images

--- bot/views.py
+++ bot/views.py
@@ -98,45 +98,7 @@ def post_carousel(reply_token):
                               }
                           ]
                         },
-                        {
-                          "thumbnailImageUrl": "https://s3-us-west-2.amazonaws.com/lineapitest/bread_240.jpeg",
-                          "title": "本格パン工房 たけよし",
-                          "text": "パンにとって一番大事だと思うものはなんですか？たけよしは、表面の焼き上がりこそが命であると考えています。食欲をそそるきつね色の絶妙な焼き目、カリッとした心地よい食感。その食感を最大限に生かすため、当店のパン生地は全て米粉。外はカリッ、中はモチモチ。世界で最も幸せなギャップをお楽しみ下さい。",
-                          "actions": [
-                          
-                              {
-                                  "type": "uri",
-                                  "label": "詳細を見る",
-                                  "uri": "https://s3-us-west-2.amazonaws.com/lineapitest/bread.jpeg"
-                              }
-                          ]
-                        },
-                        {
-                          "thumbnailImageUrl": "https://s3-us-west-2.amazonaws.com/lineapitest/harumaki_240.jpeg",
-                          "title": "ヴェトナムTokyo",
-                          "text": "1970年に東池袋にオープンしたベトナム料理の老舗。ミシュラン東京2014でも一ツ星を獲得するなど、40年以上の歴史を持ちながら、現代人の味覚に合わせたベトナム料理をご提供します。こだわりは、料理はもちろん内装にまで及び、インテリアを手掛けるのは現地では有名なあのHowell Vietnam。雰囲気まで、お召し上がり下さい。",
-                          "actions": [
-                          
-                              {
-                                  "type": "uri",
-                                  "label": "詳細を見る",
-                                  "uri": "https://s3-us-west-2.amazonaws.com/lineapitest/sushi.jpeg"
-                              }
-                          ]
-                        },
-                        {
-                          "thumbnailImageUrl": "https://s3-us-west-2.amazonaws.com/lineapitest/hamburger_240.jpeg",
-                          "title": "ジャンク・バーガー",
-                          "text": "誰が何と言おうとジャンクフードの王様は、今も昔も変わらずハンバーガー。暴力的なまでにあふれ出る肉汁。ブラックペッパーの強烈なパンチ。食べごたえ抜群の分厚いバンズ。最近流行りのヘルシー志向に反旗を翻し、ハンバーガーと本気の決闘をしたい方、とっておきの対戦相手を用意してお待ちしています。",
-                          "actions": [
-                          
-                              {
-                                  "type": "uri",
-                                  "label": "詳細を見る",
-                                  "uri": "https://s3-us-west-2.amazonaws.com/lineapitest/hamburger.jpeg"
-                              }
-                          ]
-                        },
+           
                     ]
                 }
               }
